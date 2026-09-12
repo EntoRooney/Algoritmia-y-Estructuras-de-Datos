bf542e719011353ef419c3ba4f14d46b77bfa74b
Ento Lazaro <entolazaro@gmail.com>
2026-09-11 22:15:55 -0500

    Supuesta fija

---
+++ Labs/Lab01/2026-1/FB_Recursion_Fija/Objeto.h
@@ -0,0 +1,13 @@
+//
+// Created by entol on 11/09/2026.
+//
+
+#ifndef FB_HOWARTS_OBJETO_H
+#define FB_HOWARTS_OBJETO_H
+struct Objeto {
+    int tipo;
+    int cantHechizos;
+    int duracion;
+    int ganancia;
+};
+#endif //FB_HOWARTS_OBJETO_H
